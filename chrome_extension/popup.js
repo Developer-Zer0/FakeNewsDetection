c2d10e6690d896bcca2021cb67e82c3d9a2d6d62
Developer-Zer0 <ankur.chemburkar@gmail.com>
2020-07-04 00:27:51 +0530

    chrome-extension

---
+++ chrome_extension/popup.js
@@ -0,0 +1,78 @@
+let sendText = document.getElementById('sendText');
+
+  sendText.onclick = function(element) {
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	// 	chrome.tabs.executeScript(
+	// 		tabs[0].id,
+	// 		{code: 'var div=document.createElement("div"); document.body.prepend(div); div.innerText="hey"; div.id="modal1"; div.style.backgroundColor="' + color + '"'
+	// 	});
+	// });
+	
+	var resp;
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+			null,
+			{code: "window.getSelection().toString()"}, function(selection){
+				var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+				var theUrl = "";
+				xmlhttp.open("POST", theUrl);
+				xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+				xmlhttp.send(JSON.stringify({ "text": selection[0] }));
+				xmlhttp.onreadystatechange = function() {
+					if (xmlhttp.readyState == 4) {
+					  // JSON.parse does not evaluate the attacker's scripts.
+					  resp = JSON.parse(xmlhttp.responseText);
+					}
+				}
+			}
+		);
+	});
+
+	var modal = 
+				'<div id="modal-content" class="modal">' +
+					'<div class="modal-header">' +
+						'<span id="cross" class="close" onclick="document.getElementById(`modal-content`).style.display = `none`;">&times;</span>' +
+						'<h2>Modal Header</h2>' +
+					'</div>' +
+
+					'<div class="modal-body">' +
+	  					'<p>Some text in the Modal..</p>' +
+					'</div>' +
+				'</div>'
+				
+	var modal_style = 
+				'.modal-header {' +
+					'padding: 0px 0px 16px 16px;' +
+					'background-color: #5cb85c;' +
+					'color: white;' +
+					'}' +
+				
+				'.modal-body {padding: 2px 16px;}' +
+				
+				'.modal {' +
+					'position: relative;' +
+					'background-color: #fefefe;' +
+					'margin: auto;' +
+					'padding: 0px;' +
+					'border: 1px solid #888;' +
+					'width: 50%;' +
+					'box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);' +
+					'animation-name: animatetop;' +
+					'animation-duration: 0.4s' +
+				'}' +
+				
+				'@keyframes animatetop {' +
+					'from {top: -300px; opacity: 0}' +
+					'to {top: 0px; opacity: 1}' +
+				'}'
+  
+	
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+			null,
+			{code: "var div=document.createElement('div'); document.body.prepend(div); div.innerHTML='"+modal+"'; var style=document.createElement('style'); document.body.prepend(style); style.innerHTML='"+modal_style+"';"}
+		);
+	});
+	
+  };
+  
